18ae6c44a0e1a24f9f24f3332ad36b4854e7dc11
castlem27 <marlon.castle087@gmail.com>
2025-10-07 09:09:35 +0100

    First Draft

---
+++ index.js
@@ -0,0 +1,13 @@
+var http = require("http");
+const port = 8000;
+
+http.createServer(function(req, res) {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<!DOCTYPE html> <html> <head> <title>My App</title> </head> <body> <h1>Who Am I?</h1> <p>I'm Marlon a 20 year old student at Goldsmith University</p> <h2>What Do I study?</h2> <p>I study Computing at UNIVERSITY</p> </body> </html>");
+    }).listen(port, function() {
+        console.log(`Node server is running on port ${port}...`);
+
+
+});
+
+
