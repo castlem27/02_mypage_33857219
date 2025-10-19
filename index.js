d3b211db27ed2fd24eb745304dd4b8d322e8e35f
castlem27 <marlon.castle087@gmail.com>
2025-10-19 10:52:02 +0100

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 8000;
 
 http.createServer(function(req, res) {
     res.writeHead(200, { "content-type": "text/html" });
-    res.end("<!DOCTYPE html> <html> <head> <title>My App</title> </head> <body> <h1>Who Am I?</h1> <p>I'm Marlon a 20 year old student at Goldsmith University.</p> <h2>What Do I study?</h2> <p>I study Computing at University.</p> </body> </html>");
+    res.end("<!DOCTYPE html> <html> <head> <title>My App</title> </head> <body> <h1>My Website</h1> <h2>Who Am I?</h2> <p>I'm Marlon a 20 year old student at Goldsmith University</p> <h2>What Do I Study?</h2> <p>I studdy Computing at UNIVERSITY</p> <div> <h2>Things I Like</h2> <ul> <li>Food</li> <li>Money</li> <li>Sleep</li> </ul> </div> </body> </html>");
     }).listen(port, function() {
         console.log(`Node server is running on port ${port}...`);
 
